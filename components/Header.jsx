a0cb35945cc6edd6467f5e93b7c0608e25a2fc70
Logan Dallalio <logan@dallalioweb.dev>
2022-10-04 04:10:25 -0500

    fixed toggle button

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,15 @@ import { faAlignJustify } from "@fortawesome/free-solid-svg-icons"; // import th
 import Logo from "../assests/images/logo.png";
 
 export default function Header() {
+	let toggleBtn = () => {
+		let nav = document.querySelector(".navbar");
+
+		if (nav.classList.contains("active")) {
+			nav.classList.remove("active");
+		} else {
+			nav.classList.add("active");
+		}
+	};
 	return (
 		<>
 			<Head>
@@ -12,10 +21,13 @@ export default function Header() {
 				<meta name='description' content='Sharon Dallalio Reality' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<div className='navbar'>
+			<div className='navbar '>
 				<div className='logo'>
 					<Image src={Logo} />
 				</div>
+				<button className='toggleBtn' onClick={toggleBtn}>
+					<FontAwesomeIcon icon={faAlignJustify} />
+				</button>
 				<ul>
 					<li className='navItem'>
 						<a href='#'>View Properties</a>
@@ -33,9 +45,6 @@ export default function Header() {
 				<div className='contactBtn'>
 					<a href='#'>Contact Us</a>
 				</div>
-				<button className='toggleBtn'>
-					<FontAwesomeIcon icon={faAlignJustify} />
-				</button>
 			</div>
 		</>
 	);
