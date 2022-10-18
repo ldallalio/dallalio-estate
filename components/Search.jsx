5c9cee40847b8d14ef1f996264e89f761696e0ff
Logan Dallalio <logan@dallalioweb.dev>
2022-10-18 05:02:34 -0500

    adding search bar

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,31 +1,85 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 // https://codepen.io/freshmasterj/pen/qZgVrz
 
-function openMenu(e) {
-    console.log('open')
-    const locationDropDown = document.querySelector(".dropdown")
 
-    if (locationDropDown === "hidden") {
-        
-        locationDropDown.classList.remove("hidden")
-    } else {
-        locationDropDown.classList.add("hidden");
-    }
-    
-}
+
+
 function Search() {
+    function openMenu(e) {
+        if (e.target.className == "searchByFilter") {
+        // SearchByFilter
+              const locationDropDown = document.querySelector(".dropdown")
+            if (locationDropDown.classList[2] === "hidden") {
+                locationDropDown.classList.remove("hidden")
+                document.getElementsByClassName("dropdown")[1].classList.add("hidden");
+                 document.getElementsByClassName("dropdown")[2].classList.add("hidden");
+            } else {
+                locationDropDown.classList.add("hidden");
+            }
+     } else if (e.target.className == "minPriceValue"){
+        //  Min Price
+         const locationDropDown = document.getElementsByClassName("dropdown")[1]
+            if (locationDropDown.classList[2] === "hidden") {
+                locationDropDown.classList.remove("hidden")
+                document.getElementsByClassName("dropdown")[0].classList.add("hidden");
+                 document.getElementsByClassName("dropdown")[2].classList.add("hidden");
+            } else {
+                locationDropDown.classList.add("hidden");
+            }
+     } else if (e.target.className == "maxPriceValue") {
+        // Max Price
+          const locationDropDown = document.getElementsByClassName("dropdown")[2]
+
+            if (locationDropDown.classList[2] === "hidden") {
+                locationDropDown.classList.remove("hidden")
+                document.getElementsByClassName("dropdown")[1].classList.add("hidden");
+                 document.getElementsByClassName("dropdown")[0].classList.add("hidden");
+            } else {
+                locationDropDown.classList.add("hidden");
+            }
+     }
+        
+        
+      
+    
+    }
+    useEffect(() => {
+        // SearchBy
+        document.querySelector("#searchBy").addEventListener("click", (e) => {
+            document.querySelector(".searchByFilter").innerText = e.target.innerText;
+            document.querySelector('.dropdown').classList.add("hidden")
+        })
+        // Min Price
+         document.querySelector("#minPrice").addEventListener("click", (e) => {
+            document.querySelector(".minPriceValue").innerText = e.target.innerText;
+            document.querySelector("#minPrice").classList.add("hidden");
+        })
+        // Max Price
+         document.querySelector("#maxPrice").addEventListener("click", (e) => {
+            document.querySelector(".maxPriceValue").innerText = e.target.innerText;
+            document.querySelector("#maxPrice").classList.add("hidden");
+        })
+    },[])
+   
   return (
       <>
           <div className="searchHomePage">
           <form>
                   <input className='search-input' type="search" name="search" id="id" placeholder='Ex: Baton Rouge...' />
-                  <button type='button' onClick={openMenu}><p className='search-description'>Search By</p><p>City</p></button>
-                  <ul  className='dropdown slide-top hidden'>
-                      <li>City</li>
-                      <li>Zipcode</li>  
+                  <button type='button'  onClick={openMenu}><p className='search-description'>Search By</p><p className='searchByFilter'>City</p></button>
+                  <ul id='searchBy'  className='dropdown slide-top hidden'>
+                      <li><p>City</p></li>
+                      <li><p>Zipcode</p></li>  
                   </ul>
-                  <button><p className='search-description'>Min Price</p><p>-</p></button>
-                  <button><p className='search-description'>Max Price</p><p>-</p></button>
+                  <button type='button' onClick={openMenu}><p className='search-description'>Min Price</p><p className='minPriceValue'>-</p></button>
+                  <ul  id="minPrice" className='dropdown slide-top hidden'>
+                      <li>100K</li>
+                      <li>200K</li>  
+                  </ul>
+                  <button type='button' onClick={openMenu}><p className='search-description'>Max Price</p><p className='maxPriceValue'>-</p></button><ul id="maxPrice"  className='dropdown slide-top hidden'>
+                      <li>200k</li>
+                      <li>300k</li>  
+                  </ul>
                 <button className='search-btn' type="submit">Search</button>
           </form>
         </div>
